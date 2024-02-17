567dc38cd708ddb3cc12fc75478f9f602aefaea6
demo <dazanderofficials@gmail.com>
2024-02-17 14:30:04 +0800

    added : main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+import discord
+import os
+import traceback
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = commands.Bot(
+  command_prefix = "demo.",
+  intents = discord.Intents.all(),
+  help_command = None
+)
+
+async def load():
+  for folder in os.listdir('cogs'):
+    for file in os.listdir(f'cogs/{folder}'):
+      if file.endswith(".py"):
+        await bot.load_extension(f'cogs.{folder}.{file[:-3]}')
+
+async def main():
+  await load()
+  await bot.start(os.getenv("TOKEN"), reconnect = True)
+
+try:
+  asyncio.run(main())
+except:
+  traceback.print_exc()
